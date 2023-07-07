7f0489997ebfee05e004b8a6181337b2aca20e38
fifmogi <fifmogi@gmail.com>
2023-07-08 03:55:18 +0700

    fixing initilal commit

---
+++ Global.gd
@@ -0,0 +1,27 @@
+extends Node
+
+var current_camera = null
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func camera_shake(strength, time):
+	if(current_camera != null):
+		current_camera.camera_shake(strength, time);
+
+func play_SFX(audio, bus:String="Master"):
+#	if !audio_player.is_playing() and Global.can_playSFX:
+#	Global.can_playSFX = false
+	var audio_player = AudioStreamPlayer.new()
+	add_child(audio_player) 
+	audio_player.stream = audio
+	audio_player.set_bus(bus)
+	audio_player.play()
+	await audio_player.finished
+	audio_player.queue_free()
