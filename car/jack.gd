2c9a688685e0d78bc8a5b9d7106b1b5b2ad2fcf4
Sasha Pomosov <v.aleksandr.pomosov@reddit.com>
2023-07-08 01:03:00 -0400

    Car components, tool slots

--- car/jack.gd
+++ car/jack.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var label = $Label
+@onready var work_timer = $WorkTimer
+@onready var progress_bar = $ProgressBar
 
 var activated = false
 
@@ -16,13 +18,28 @@ func _process(delta):
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-		activated = !activated
 		toggle_jack()
 		
 		
 func toggle_jack():
-	if activated:
-		label.text = "Jack ON"
+	if !activated:
+		progress_bar.value = 0
+		work_timer.start(0.25)
+
 	else:
+		activated = false
+		progress_bar.value = 0
+		work_timer.stop()
 		label.text = "Jack OFF"
 
+
+
+func _on_work_timer_timeout():
+	progress()
+
+
+func progress():
+	progress_bar.value += 10
+	if progress_bar.value == 100:
+		activated = true
+		label.text = "Jack ON"
