48f48ec79c8e13b9710d9e72f4d4f5753a08ce94
Sasha Pomosov <v.aleksandr.pomosov@reddit.com>
2023-07-08 01:22:57 -0400

    Restore main scene

--- tools/Tool.gd
+++ tools/Tool.gd
@@ -2,7 +2,7 @@ extends Node2D
 class_name Tool
 
 var is_ready = true
-enum ToolType {FIRE_EXTINGUISHER, SCREWDRIVER, JACK, TIRE}
+enum ToolType {FIRE_EXTINGUISHER, SCREWDRIVER, JACK, TYRE}
 @export var type: ToolType
 
 # Called when the node enters the scene tree for the first time.
